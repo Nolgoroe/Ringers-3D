3aa72f0fdd66c9237ba2973e99d83693be5b196b
Avishy <Avishy.menashe@gmail.com>
2020-10-14 19:00:00 +0300

    Season 1 - Chapter 1
    Follow Mouse
    Spawn board and clip
    Drag Pieces and place them on board

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public GameObject circleBoardPrefab;
+    public GameObject clipPrefab;
+
+    public GameObject gameBoard;
+    public GameObject gameClip;
+
+    public Transform destroyOutOfLevel;
+
+    public CursorController cursorControl;
+
+    void Start()
+    {
+        Instance = this;
+
+        gameBoard = Instantiate(circleBoardPrefab, destroyOutOfLevel);
+        gameClip = Instantiate(clipPrefab, destroyOutOfLevel);
+
+        cursorControl.Init();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
