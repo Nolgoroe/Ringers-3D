59500765c65aad48c76259bada6f415790edaba2
Avishy <avishy.menashe@gmail.com>
2021-04-25 04:54:04 +0300

    Tutorial problem solved

---
+++ Assets/CutoutMaskUIScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class CutoutMaskUIScript : Image
+{
+    public override Material materialForRendering
+    {
+        get
+        {
+            Material mat = new Material(base.materialForRendering);
+            mat.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return mat;
+        }
+    }
+}
