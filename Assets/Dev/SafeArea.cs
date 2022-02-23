3f7f69341a4374cf50979a9d082fb6a404f4e286
Avishy <Avishy.menashe@gmail.com>
2022-02-23 23:35:59 +0100

    safe are bug

--- Assets/Dev/SafeArea.cs
+++ Assets/Dev/SafeArea.cs
@@ -9,20 +9,20 @@ public class SafeArea : MonoBehaviour
     Vector2 minAnchor;
     Vector2 maxAnchor;
 
-    private void Awake()
-    {
-        rectTransform = GetComponent<RectTransform>();
-        safeArea = Screen.safeArea;
-        minAnchor = safeArea.position;
-        maxAnchor = minAnchor + safeArea.size;
+    //private void Awake()
+    //{
+    //    rectTransform = GetComponent<RectTransform>();
+    //    safeArea = Screen.safeArea;
+    //    minAnchor = safeArea.position;
+    //    maxAnchor = minAnchor + safeArea.size;
 
-        minAnchor.x /= Screen.width;
-        minAnchor.y /= Screen.height;
+    //    minAnchor.x /= Screen.width;
+    //    minAnchor.y /= Screen.height;
 
-        maxAnchor.x /= Screen.width;
-        maxAnchor.y /= Screen.height;
+    //    maxAnchor.x /= Screen.width;
+    //    maxAnchor.y /= Screen.height;
 
-        rectTransform.anchorMin = minAnchor;
-        rectTransform.anchorMax = maxAnchor;
-    }
+    //    rectTransform.anchorMin = minAnchor;
+    //    rectTransform.anchorMax = maxAnchor;
+    //}
 }
